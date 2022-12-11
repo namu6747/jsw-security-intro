91a71731552edd8a003951da8d766b9463720052
namu6747 <namu6747@naver.com>
2022-12-11 17:22:19 +0900

    add SecurityConfig.java, set security user

---
+++ basicsecuritynew/src/main/java/io/security/basicsecuritynew/SecurityConfig.java
@@ -0,0 +1,22 @@
+package io.security.basicsecuritynew;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+            .authorizeRequests()
+               .anyRequest().authenticated() // 어떤 요청에도 인증을 받겠다.
+            .and()
+                .formLogin(); // formLogin 방식으로 인증하겠다.
+        return http.build();
+    }
+}

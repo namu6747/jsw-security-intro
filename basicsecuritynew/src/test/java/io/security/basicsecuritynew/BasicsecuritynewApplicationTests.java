f6002fcb7ad66b3418864c1dd1c0424230190ed6
namu6747 <namu6747@naver.com>
2022-12-11 17:05:02 +0900

    init projs

---
+++ basicsecuritynew/src/test/java/io/security/basicsecuritynew/BasicsecuritynewApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.basicsecuritynew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicsecuritynewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
